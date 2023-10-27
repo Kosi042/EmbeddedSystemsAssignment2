fed8c18061f3e2209c002ede096bc16fc5a04669
Filip Lobpreis <filob23@student.sdu.dk>
2023-10-27 14:31:06 +0200

    subscriber: added to cmake

--- src/assignment2/src/subscriber.cpp
+++ src/assignment2/src/subscriber.cpp
@@ -1,7 +1,7 @@
 #include <memory>
 #include <iostream>
 
-#include <cv_bridge/cv_bridge.h> 
+#include <cv_bridge/cv_bridge.h>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp> //use image.hpp dont know if correct
 using std::placeholders::_1;
@@ -19,11 +19,11 @@ class MinimalSubscriber : public rclcpp::Node
   private:
     void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
     {
-      std::cout << "H: " << msg->height << "W: " << msg->width << std::endl;
       cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
 	  cv::Mat img = cv_ptr->image;
       cv::Mat img_small;
-      resize(img, img_small, Size(300, 200), INTER_LINEAR);
+      resize(img, img_small, cv::Size(300, 200), cv::INTER_LINEAR);
+      std::cout << "H: " << img_small.rows << "W: " << img_small.cols << std::endl;
     }
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
 };
