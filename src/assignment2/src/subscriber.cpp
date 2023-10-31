e813f1a095e94a4a1eb648226c18b68ba1520fca
Filip Lobpreis <filob23@student.sdu.dk>
2023-10-31 15:19:06 +0100

    assignment2: image publisher

--- src/assignment2/src/subscriber.cpp
+++ src/assignment2/src/subscriber.cpp
@@ -1,48 +1,77 @@
 #include <memory>
 #include <iostream>
-
 #include <cv_bridge/cv_bridge.h>
 #include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/msg/image.hpp> //use image.hpp dont know if correct
-using std::placeholders::_1;
+#include <sensor_msgs/msg/image.hpp>
 
-#define FLATTENED_MINIMIYED_IMAGE 200 * 300 * 3
+#include "drivers/ximge_processor.h" // Include the header for your XImge_processor
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
 
 class MinimalSubscriber : public rclcpp::Node
 {
-  public:
-    MinimalSubscriber()
-    : Node("img_subscriber")
-    , toFpga{0}
-    {
-      subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-      "/image_raw", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-    }
+public:
+	MinimalSubscriber()
+		: Node("img_subscriber")
+	{
+		publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/processed_image", 10);
 
-  private:
-    void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
-    {
-      cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
-	  cv::Mat img = cv_ptr->image;
-      cv::Mat img_small;
-      resize(img, img_small, cv::Size(300, 200), cv::INTER_LINEAR);
-      std::cout << "H: " << img_small.rows << "W: " << img_small.cols << std::endl;
-      auto storage = std::vector<uchar>(FLATTENED_MINIMIYED_IMAGE); // or however many channels you have
-      auto mat     = cv::Mat_<uchar>(200, 300, CV_8UC3, storage.data());
-      auto tmp = vector<uchar>(img.begin<uchar>(), img.end<uchar>());
-      toFpga = &tmp[0];
+		subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+			"/image_raw", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
 
+		// Initialize the XImge_processor
+		int status = XImge_processor_Initialize(&ip_inst, "imge_processor");
+		if (status != XST_SUCCESS) {
+			RCLCPP_ERROR(this->get_logger(), "Error: Could not initialize the IP core.");
+			return;
+		}
 
-    }
+		// Resize the output image to match your requirements
+		out_image_.height = 100;
+		out_image_.width = 150;
+		out_image_.encoding = "bgr8"; // Update with the actual encoding
+		out_image_.step = 150 * 3; // Assuming 3 channels
+		out_image_.data.resize(out_image_.height * out_image_.step);
+	}
 
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
-    unsigned char toFpga[FLATTENED_MINIMIYED_IMAGE];
+	~MinimalSubscriber()
+	{
+		XImge_processor_Release(&ip_inst);
+	}
+
+private:
+	void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg)
+	{
+		cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
+		cv::Mat img = cv_ptr->image;
+		
+		cv::Mat img_small;
+		resize(img, img_small, cv::Size(150, 100), cv::INTER_LINEAR);
+
+		std::cout << "H: " << img_small.rows << " W: " << img_small.cols << std::endl;
+
+		// Perform processing on img_small
+
+		// For the sake of demonstration, let's assume you copy the processed image to out_image_
+		std::memcpy(out_image_.data.data(), img_small.data, out_image_.data.size());
+
+		// Publish the processed image
+		publisher_->publish(out_image_);
+	}
+
+	rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+	rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+	sensor_msgs::msg::Image out_image_;
+
+	XImge_processor ip_inst;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
-  rclcpp::shutdown();
-  return 0;
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<MinimalSubscriber>());
+	rclcpp::shutdown();
+	return 0;
 }
+
