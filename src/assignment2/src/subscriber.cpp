3d271ac53f6796d21f19a3800eb3fad8df8f060b
Filip Lobpreis <filob23@student.sdu.dk>
2023-11-03 16:41:40 +0100

    subscriber

--- src/assignment2/src/subscriber.cpp
+++ src/assignment2/src/subscriber.cpp
@@ -41,20 +41,52 @@ public:
 	}
 
 private:
+	cv::Mat vectorToMat(const std::vector<uint32_t>& inputVector, int rows, int cols) {
+		cv::Mat image(rows, cols, CV_8UC1);
+
+		if (static_cast<int>(inputVector.size()) != rows * cols) {
+			std::cerr << "Input vector size does not match the specified rows and columns." << std::endl;
+			return cv::Mat(); // Return an empty Mat in case of an error.
+		}
+
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				uint32_t pixelValue = inputVector[i * cols + j] & 0xFF; // Extract the least significant byte.
+				image.at<uchar>(i, j) = static_cast<uchar>(pixelValue);
+			}
+		}
+
+		return image;
+	}
+
 	void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg)
 	{
 		cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
 		cv::Mat img = cv_ptr->image;
-		
+
 		cv::Mat img_small;
-		resize(img, img_small, cv::Size(150, 100), cv::INTER_LINEAR);
+		resize(img, img_small, cv::Size(32, 24));
 
 		std::cout << "H: " << img_small.rows << " W: " << img_small.cols << std::endl;
 
+		cv::Mat grayscaleImage;
+		cv::cvtColor(img_small, grayscaleImage, cv::COLOR_BGR2GRAY);
 		// Perform processing on img_small
 
 		// For the sake of demonstration, let's assume you copy the processed image to out_image_
-		std::memcpy(out_image_.data.data(), img_small.data, out_image_.data.size());
+		cv::Mat flattenedVector = grayscaleImage.reshape(1, 1);
+		std::vector<word_type> flattenedStdVector;
+		flattenedStdVector.assign(flattenedVector.data, flattenedVector.data + flattenedVector.total());
+		word_type *data = flattenedStdVector.data();
+		XImge_processor_Write_in_r_Words(ip_inst, 0, data, flattenedStdVector.size());
+
+		while (!XImge_processor_IsDone(ip_inst));
+
+		XImge_processor_Read_out_r_Words(ip_inst, 0, data, DATA_SIZE);
+
+		auto filteredImg = vectorToMat(flattenedStdVector, 32, 24);
+
+		out_image_ = cv_brige::CvImage(std_msgs::Header(), "mono8", filteredImg)
 
 		// Publish the processed image
 		publisher_->publish(out_image_);
